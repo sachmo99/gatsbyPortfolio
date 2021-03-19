9aac3530c49fefc7a8c3ddede8d85acb0e1d1a54
sachmo99 <tsaimouli1999@gmail.com>
2021-03-20 03:25:57 +0530

    updated config gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     author: 'Sai Chandra Mouli',
     description: 'Portfolio site built with Gatsby, React with template from Knochenmark(github)',
     url: 'https://sachmo99.github.io',
-    pathPrefix: '/portfolio',
+    //pathPrefix: '/gatsbyPortfolio',
     paginationPageSize: 4, // Amount of posts displayed per listing page.
   },
   plugins: [
