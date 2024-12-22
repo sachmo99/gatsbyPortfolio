d99e151dc3fc2d959fc83761916574f513811811
Sai Chandra Mouli T <tsaimouli1999@gmail.com>
2024-12-23 01:48:26 +0530

    changed browserrouter to hashrouter for gh-apges support

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { HashRouter as Router,Route,Routes } from 'react-router-dom';
 import ContactPage from './sections/contactme.tsx';
 import AboutPage from './sections/aboutme.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/aboutme" element={<AboutPage />} />
         <Route path="/contactme" element={<ContactPage />} />
         <Route path="*" element={<div><h1>Not Found!</h1><p>Go to to <a href="/">HOME</a></p></div>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
     </StrictMode>,
 )
